8d8e27f5d462b524c48fa5a76dc210a44d7d851b
Devin Moore <115327949+DevinSocial@users.noreply.github.com>
2022-10-15 18:30:52 -0400

    Update

---
+++ intro.js
@@ -0,0 +1,45 @@
+let header = document.querySelector('#intro');
+let anim = [
+    { t: "{ }", ms: 200 },
+    { t: "{_}", ms: 200 },
+    { t: "{ }", ms: 200 },
+    { t: "{_}", ms: 200 },
+    { t: "{D_}", ms: 100 },
+    { t: "{DE_}", ms: 100 },
+    { t: "{DEV_}", ms: 100 },
+    { t: "{DEV_}", ms: 100 },
+    { t: "{DEVI_}", ms: 100 },
+    { t: "{DEVIN_}", ms: 100 },
+    { t: "{DEVIN M_}", ms: 100 },
+    { t: "{DEVIN MO_}", ms: 100 },
+    { t: "{DEVIN MOO_}", ms: 100 },
+    { t: "{DEVIN MOOR_}", ms: 100 },
+    { t: "{DEVIN MOORE_}", ms: 100 },
+    { t: "{DEVIN MOORE }", ms: 200 },
+    { t: "{DEVIN MOORE_}", ms: 200 },
+    { t: "{DEVIN MOORE }", ms: 200 },
+    { t: "{DEVIN MOORE_}", ms: 200 },
+    { t: "{DEVIN MOORE}", ms: 200 },
+    { t: "{DEVIN MOORE}", ms: 200 }
+];
+let stepDenominator = 1;
+if (window.localStorage.stepDenominator)
+    stepDenominator = window.localStorage.stepDenominator;
+let i = 0;
+let update = () => {
+    let step = anim[i];
+    header.innerText = step.t;
+    i++;
+
+    if (i < anim.length)
+        setTimeout(update, step.ms / stepDenominator);
+    else {
+        header.classList.add('top');
+        setTimeout(() => {
+            document.getElementById('main').style.opacity = 1;
+            initGlobe();
+        }, 500);
+        window.localStorage.stepDenominator = 2;
+    }
+}
+update();
