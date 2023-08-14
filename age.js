daf911540ad89cd8f5feb5d918ee3b7ba09ecf19
Devin Moore <115327949+DevinSocial@users.noreply.github.com>
2023-08-13 21:37:29 -0400

    Delete age.js

--- age.js
+++
@@ -1,6 +0,0 @@
-let ageEl = document.getElementById("age");
-
-setInterval(() => {
-	let time = (new Date() - new Date(1026792000000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
-	ageEl.innerText = time.toString().substring(0, 12);
-}, 50);
